9deff8f7a661b1d5dd45f2e0f82f374a581354f0
kaiocon <kaiocon@users.noreply.github.com>
2020-03-28 18:23:59 +0000

    bugfix

---
+++ client/build/precache-manifest.9a79d46dbf485af14950aa8a551952db.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe846f87e24e3a5be13a5f3290e7c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08f2c7688ddafcf76863",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5a32e570ace910f09d3c",
+    "url": "/static/css/main.bf21efbc.chunk.css"
+  },
+  {
+    "revision": "08f2c7688ddafcf76863",
+    "url": "/static/js/2.fcf49a09.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/static/js/2.fcf49a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a32e570ace910f09d3c",
+    "url": "/static/js/main.b6fc6ee2.chunk.js"
+  },
+  {
+    "revision": "47558488fe04bdba6225",
+    "url": "/static/js/runtime-main.66981f3b.js"
+  },
+  {
+    "revision": "5739940da79d5fa8e595e64c05669c2a",
+    "url": "/static/media/Belgium.5739940d.svg"
+  },
+  {
+    "revision": "0efb6bf1f18132e87ea2c57d9580c45c",
+    "url": "/static/media/Canada.0efb6bf1.svg"
+  },
+  {
+    "revision": "e18b270f56f90ad1f19660e70b68fb3a",
+    "url": "/static/media/England.e18b270f.svg"
+  },
+  {
+    "revision": "4e3ec048e64c77815332dbb34f9b6305",
+    "url": "/static/media/France.4e3ec048.svg"
+  },
+  {
+    "revision": "246a5fcba5ed21747c2a108e5e635ec7",
+    "url": "/static/media/Germany.246a5fcb.svg"
+  },
+  {
+    "revision": "feb9eea9ce02f8633ea8c37354a7e1cb",
+    "url": "/static/media/Netherlands.feb9eea9.svg"
+  },
+  {
+    "revision": "c4361672853bbab112bd4b360e6dd199",
+    "url": "/static/media/Scotland.c4361672.svg"
+  },
+  {
+    "revision": "da1c4f85e66e46f759fe736e3f2a5b37",
+    "url": "/static/media/USA.da1c4f85.svg"
+  },
+  {
+    "revision": "0399e8014aa6e37ee3588d1851c59b5f",
+    "url": "/static/media/brackets.0399e801.svg"
+  },
+  {
+    "revision": "e51fb6ea782545cbdea27857dec09db8",
+    "url": "/static/media/hero.e51fb6ea.png"
+  },
+  {
+    "revision": "6ff36fdd0b18ac0c951e91640c75f33c",
+    "url": "/static/media/logo.6ff36fdd.svg"
+  },
+  {
+    "revision": "ca2a72fb0cefaffa80749113b8495851",
+    "url": "/static/media/logo2.ca2a72fb.svg"
+  },
+  {
+    "revision": "0cd5e3c15640413c3964150c44ae2c80",
+    "url": "/static/media/pay.0cd5e3c1.svg"
+  },
+  {
+    "revision": "d0a5c7b27f6647275d1a36e043096c89",
+    "url": "/static/media/score.d0a5c7b2.svg"
+  }
+]);
